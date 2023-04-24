f353a00fedef6650e3e111f3c485d5a0b621f259
choijungwoo493 <jwoo1016@gmail.com>
2023-04-24 18:19:08 +0900

    initial commit

---
+++ src/main/java/com/project/wish/WishApplication.java
@@ -0,0 +1,13 @@
+package com.project.wish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/wish/WishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
