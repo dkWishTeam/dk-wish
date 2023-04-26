8fdfb0831b8b66de81dfd501f6ee4fc473ee2eb6
choijungwoo493 <jwoo1016@gmail.com>
2023-04-26 16:17:42 +0900

    ADD DB config file

---
+++ src/main/java/com/project/wish/config/DateSourceConfig.java
@@ -0,0 +1,39 @@
+package com.project.wish.config;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * DB 연동을 위한 클래스로 DataSource bean 을 설정합니다.
+ */
+@Configuration
+public class DateSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource(){
+        System.out.println(driverClassName);
+        return DataSourceBuilder.create()
+            .url(url)
+            .username(username)
+            .password(password)
+            .driverClassName(driverClassName)
+            .build();
+    }
+
+
+}
