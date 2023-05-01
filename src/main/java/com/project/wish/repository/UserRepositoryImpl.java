6164596afd0abf277adab8750cac1ed03be06fe2
choijungwoo493 <jwoo1016@gmail.com>
2023-05-01 17:38:15 +0900

    등록 수정 삭제 의 return 값을 boolean 이 아닌 void 로 변경했고 매개변수명을 일부 변경했습니다. 또한 메서드를 추가하고 완성했습니다.

--- src/main/java/com/project/wish/repository/UserRepositoryImpl.java
+++ src/main/java/com/project/wish/repository/UserRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.project.wish.repository;
 
 import com.project.wish.domain.User;
+import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -12,50 +13,68 @@ public class UserRepositoryImpl implements UserRepository {
     SqlSession session;
 
     @Override
-    public boolean insertUser(User user) {
-        String statement = "lab1.exam1";
-        if(session.insert(statement,user) == 1){
-            return true;
-        }
-        return false;
+    public void insertUser(User user) {
+        String statement = "wishdb.insertUser";
+        session.insert(statement, user);
     }
 
     @Override
-    public boolean deleteUser(int userId) {
-        String statement = "lab1.exam1";
-        if(session.insert(statement,user) == 1){
-            return true;
-        }
-        return false;
+    public User findUserById(Integer id) {
+        String statement = "wishdb.findUserById";
+        return session.selectOne(statement, id);
     }
 
     @Override
-    public boolean updateUser(User user) {
-        return false;
+    public User findUserByIdByAdmin(Integer id) {
+        String statement = "wishdb.findUserByIdByAdmin";
+        return session.selectOne(statement, id);
     }
 
     @Override
-    public boolean findUser(int userId) {
-        return false;
+    public List<User> findUsers() {
+        String statement = "wishdb.findUsers";
+        return session.selectOne(statement);
     }
 
     @Override
-    public boolean findUsers() {
-        return false;
+    public void updateUser(User user) {
+        String statement = "wishdb.updateUser";
+        session.update(statement, user);
     }
 
     @Override
-    public User findByEmail(String email) {
-        return null;
+    public void updateUserByAdmin(Integer id) {
+        String statement = "wishdb.updateUserByAdmin";
+        session.update(statement, id);
     }
 
     @Override
-    public User findByNickname(String nickname) {
-        return null;
+    public void deleteUser(int id) {
+        String statement = "wishdb.deleteUser";
+        session.delete(statement, id);
     }
 
     @Override
-    public User findByPhone(String phone) {
-        return null;
+    public User findUserByPhone(String phone) {
+        String statement = "wishdb.findUserByPhone";
+        return session.selectOne(statement, phone);
+    }
+
+    @Override
+    public User findUserByUserId(String userId) {
+        String statement = "wishdb.findUserByUserId";
+        return session.selectOne(statement, userId);
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        String statement = "wishdb.findUserByEmail";
+        return session.selectOne(statement, email);
+    }
+
+    @Override
+    public User findUserByNickname(String nickname) {
+        String statement = "wishdb.findUserByNickname";
+        return session.selectOne(statement, nickname);
     }
 }
