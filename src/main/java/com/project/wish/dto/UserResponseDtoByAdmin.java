a36be660ba16aa08205968ad5aa57dd9dd10c82c
choijungwoo493 <jwoo1016@gmail.com>
2023-05-01 17:36:16 +0900

    user 의 값을 사용자에게 변환해줄 때 쓰이는 responseDto 를 만들었습니다.

--- src/main/java/com/project/wish/dto/UserResponseDtoByAdmin.java
+++ src/main/java/com/project/wish/dto/UserResponseDtoByAdmin.java
@@ -1,5 +1,19 @@
 package com.project.wish.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDtoByAdmin {
 
+    private Integer id;
+    private String nickname;
+    private boolean isBlock;
 }
