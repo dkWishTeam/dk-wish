da078c21f888dac9b6f43fa5a7be2190cc61098f
choijungwoo493 <jwoo1016@gmail.com>
2023-05-01 17:30:48 +0900

    dto 를 쓸 때 필요한 어노테이션을 추가했습니다.

--- src/main/java/com/project/wish/dto/UserUpdateRequestDto.java
+++ src/main/java/com/project/wish/dto/UserUpdateRequestDto.java
@@ -1,8 +1,15 @@
 package com.project.wish.dto;
 
 import java.util.Date;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Builder
 public class UserUpdateRequestDto {
+
     private String userId;
     private String password;
     private String email;
