1195f62d4712ed3e0a7772b86392341a6158ce69
choijungwoo493 <jwoo1016@gmail.com>
2023-05-01 17:30:07 +0900

    업데이트를 할 때 userId 를 쓰므로 domain 변경을 해줬습니다.

--- src/main/java/com/project/wish/domain/User.java
+++ src/main/java/com/project/wish/domain/User.java
@@ -3,12 +3,14 @@ package com.project.wish.domain;
 import java.time.LocalDateTime;
 import java.util.Date;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Setter;
 
 @Setter
+@Getter
 @Builder
 public class User {
-
+    private Integer id;
     private int roleId;
     private String userId;
     private String password;
