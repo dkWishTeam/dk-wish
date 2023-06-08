bc3dc609e3e5bb8c60191bc733e50294ba371ec7
choijungwoo493 <jwoo1016@gmail.com>
2023-06-08 16:29:38 +0900

    jwt 토큰의 만료기간 설정

--- src/main/java/com/project/wish/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/project/wish/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.project.wish.filter;
 
+import com.google.gson.JsonObject;
 import com.project.wish.jwt.JwtTokenProvider;
 import java.io.IOException;
 import javax.servlet.FilterChain;
@@ -8,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,12 +25,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
         String jwt = getJwtFromRequest(request);
-        if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
-            // 2. 로직이 맞나 확인
-            Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (StringUtils.hasText(jwt)) {
+            if (jwtTokenProvider.validateToken(jwt)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                filterChain.doFilter(request, response);
+            } else {
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("error", "Expired or invalid JWT token");
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");  // 추가되는 부분
+                response.getWriter().write(jsonObject.toString());
+                return;
+            }
+        } else {
+            filterChain.doFilter(request, response);
         }
-        filterChain.doFilter(request, response);
     }
 
     private String getJwtFromRequest(HttpServletRequest request) {
