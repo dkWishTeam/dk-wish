a610f400f0b7e0bc1b58fcf6a9798c336c4f532c
choijungwoo493 <jwoo1016@gmail.com>
2023-06-11 13:27:37 +0900

    예외공통처리 클래스 rest 형식으로 변경

--- src/main/java/com/project/wish/advice/ExceptionHandlerAdvice.java
+++ src/main/java/com/project/wish/advice/ExceptionHandlerAdvice.java
@@ -1,35 +1,37 @@
 package com.project.wish.advice;
 
-import com.project.wish.exception.NotFoundWishHistoryException;
-import com.project.wish.exception.SignUpFailException;
 import com.project.wish.exception.UnAuthorizedAccessException;
-import org.springframework.stereotype.Component;
-import org.springframework.ui.Model;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-@Component
 public class ExceptionHandlerAdvice {
 
-    @ExceptionHandler(SignUpFailException.class)
-    public String handleSignUpFailException(SignUpFailException ex, Model model) {
-        String errorMessage = ex.getMessage();
-        model.addAttribute("errorMessage", errorMessage);
-        return "error/4xx";
-    }
+    public static final String MESSAGE = "message";
+    public static final String TIMESTAMP = "timestamp";
 
     @ExceptionHandler(UnAuthorizedAccessException.class)
-    public String handleUnAuthorizedAccessException(UnAuthorizedAccessException ex, Model model) {
-        String errorMessage = ex.getMessage();
-        model.addAttribute("errorMessage", errorMessage);
-        return "error/4xx";
+    public ResponseEntity<?> handleUnAuthorizedAccessException(UnAuthorizedAccessException e) {
+        // 예외에 대한 상세 정보를 담을 객체 생성
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put(MESSAGE, e.getMessage());
+        errorDetails.put(TIMESTAMP, LocalDateTime.now());
+
+        // 에러 메시지와 함께 HTTP 상태 코드 400(Bad Request) 반환
+        return new ResponseEntity<>(errorDetails, HttpStatus.FORBIDDEN);
     }
 
-    @ExceptionHandler(NotFoundWishHistoryException.class)
-    public String notFoundWishHistoryException(NotFoundWishHistoryException ex, Model model) {
-        String errorMessage = ex.getMessage();
-        model.addAttribute("errorMessage", errorMessage);
-        return "error/4xx";
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneralException(Exception e) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put(MESSAGE, e.getMessage());
+        errorDetails.put(TIMESTAMP, LocalDateTime.now());
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
